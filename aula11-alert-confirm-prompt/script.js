1d14b21ec27740017f02ab909db08a30f6f83f4e
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-05-20 11:39:06 +0200

    Atualização aula 11

---
+++ aula11-alert-confirm-prompt/script.js
@@ -0,0 +1,9 @@
+let num1 = Number (prompt('Digite um némero:'));
+let num2 = Number (prompt('Digite outro número:'));
+// ou se pode fazer assim:
+//num1 = Number(num1);
+//alert(`O resultado é ${num1 + num2} `); ou
+
+const resultado = num1 + num2
+alert(`O resultado é ${resultado} `);
+   
