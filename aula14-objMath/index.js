f2cd097247d42aeb07442ef08e11660e7bb0675a
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-05-21 12:11:36 +0200

    Criei aula 14 Obejto Math

---
+++ aula14-objMath/index.js
@@ -0,0 +1,27 @@
+/*let num1 = 9.54236;
+//let num2 = Math.floor(num1); // Arredonda pra baixo
+//let num2 = Math.ceil(num1); // Arredonda pra cima
+let num2 = Math.round(num1); // Arredonda para o nº mais próximo
+console.log(num2);*/
+
+
+let num1 = 16;
+console.log(num1 ** 0.5); // Raiz quadrada
+
+console.log(Math.max(1, 2, 3, 5, -10, -50, 1500, 9, 8, 7, 6)); // maior
+
+console.log(Math.min(1, 2, 3, 5, -10, -50, 1500, 9, 8, 7, 6)); // menor
+
+const aleatorio = Math.round(Math.random() * (10 - 0) + 0); // nº aleatório
+console.log(aleatorio);
+
+
+console.log(Math.PI); // Valor de PI
+
+console.log(Math.pow(2, 10)); // Potência
+console.log(2 ** 10); // Potência, modo simples de fazer
+
+
+
+
+
