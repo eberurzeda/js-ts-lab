4b1ae566f895d4c4c2c5f32f05531516196adbe4
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-05-25 11:29:27 +0200

    Criei a aula 21 Operadores de Comparação

---
+++ aula21operadoresComparação/index.js
@@ -0,0 +1,25 @@
+/*
+Operadores de Comparação:
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou menor a
+== igualdade (valor) 'quase não se usa'.
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) 'quase não se usa'.
+!== diferente estrito (valor e tipo)
+
+//const comp = 10 >= 5;
+//console.log(comp);
+
+const num1 = 10; //Number
+const num2 = '10'; // String
+const comp = num1 === num2; // Number é igual a String?f alse
+console.log(comp);
+//Obs.: Se o sinal fosse == só com 'dois =', daria verdadeiro, pois esse sinal não difere tipos, só valores*/
+
+const num1 = 10; //Number
+const num2 = '10'; // String
+const comp = num1 !== num2; // Number é diferente a String?f alse
+console.log(comp);
+
