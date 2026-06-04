0a9bc934d6b994ae09d116b43b089e6c82c8b132
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-06-04 10:51:24 +0200

    Criei a aula 28 Objeto Date

---
+++ aula28ObjetoDate/index.js
@@ -0,0 +1,34 @@
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours() + 1);
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+    
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
+
+
+/*const data = new Date();
+console.log(data.toString());
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth() + 1); //Mês começa do zero
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('MSe', data.getMilliseconds());
+console.log('Dia semana', data.getDay()); // 0 = domingo e 6 é sábado
+console.log(Date.now());*/
+
+
