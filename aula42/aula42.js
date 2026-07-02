2ffc555489713b1eec0bff18dea724ebc3c38997
Eber Urzeda dos Santos <eber.urzeda@gmail.com>
2026-07-02 20:46:25 +0200

    Criei aula42

---
+++ aula42/aula42.js
@@ -0,0 +1,12 @@
+/*function ePaisagem (larg, alt) {
+    return larg > alt;
+}
+
+console.log(ePaisagem(1920, 1080));*/
+
+//(x, y) => x > y ? x : y;
+
+const ePaisagem = (larg, alt) =>
+    larg > alt;
+
+console.log(ePaisagem(1920, 1080));
